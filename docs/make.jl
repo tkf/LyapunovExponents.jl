37faf44bbf7fbe57876e2c1339f8570730d04b7b
Takafumi Arakaki <aka.tkf@gmail.com>
2018-01-21 17:29:57 -0800

    Build and deploy document via Travis

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,9 @@
 using Documenter, LyapunovExponents
 
 makedocs()
+
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-alabaster"),
+    repo   = "github.com/tkf/LyapunovExponents.jl.git",
+    julia  = "0.6",
+)
