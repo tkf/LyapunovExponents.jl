e943822442aba8f01622d4ac9ee24a6f748752b4
Takafumi Arakaki <aka.tkf@gmail.com>
2018-01-25 00:16:06 -0800

    Use tangent_dynamics! as the keyword argument

--- src/types.jl
+++ src/types.jl
@@ -66,12 +66,12 @@ struct LEProblem{DEP} <: AbstractLEProblem
             num_tran=1,
             dim_lyap=dimension(phase_prob),
             Q0=eye(dimension(phase_prob), dim_lyap),
-            tangent_dynamics=nothing,
+            tangent_dynamics! = nothing,
             ) where {DEP}
         if ! is_semi_unitary(Q0)
             error("Columns in Q0 are not orthonormal.")
         end
-        new{DEP}(phase_prob, num_tran, dim_lyap, Q0, tangent_dynamics)
+        new{DEP}(phase_prob, num_tran, dim_lyap, Q0, tangent_dynamics!)
     end
 end
 
