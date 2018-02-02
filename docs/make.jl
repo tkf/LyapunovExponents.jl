5dfe6b4256e6a02e2297a2c3599c83099275496b
Takafumi Arakaki <aka.tkf@gmail.com>
2018-02-01 22:37:59 -0800

    Skip plotting in non-master branch

--- docs/make.jl
+++ docs/make.jl
@@ -3,13 +3,20 @@ using Documenter, LyapunovExponents
 using Plots
 gr()
 
+function should_plot(env = ENV)
+    (@show get(env, "TRAVIS_BRANCH", "")) == "master" &&
+        (@show get(env, "TRAVIS_PULL_REQUEST", "false")) == "false"
+end
+
 # Generate src/gallery/examples/*.{md,png}.  In principle, I can
 # generate those files in build/gallery/examples/ after `makedocs()`
 # below.  However, resolving @ref etc. are done by `makedocs()` so
 # it's better to generate Markdown files here.
 cd(dirname(@__FILE__)) do
     run(`make gallery_md`)
-    include("plot_gallery.jl")
+    if should_plot()
+        include("plot_gallery.jl")
+    end
 end
 
 makedocs()
