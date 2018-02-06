4ce71c0da74715615d9836736fad015e94f49a7b
autodocs <autodocs>
2018-02-06 13:41:55 +0000

    build based on c022d71

---
+++ latest/gallery/Ginelli_2007_Figure_1a.jl
@@ -0,0 +1,22 @@
+using LyapunovExponents
+
+function get_angles(solver)
+    return [acos(abs(dot(C[:, 1], C[:, 2]))) * 2 / π for C
+            in backward_dynamics!(solver)]
+end
+
+henon_demo = LyapunovExponents.henon_map(num_attr=100000)
+henon_demo.prob :: LEProblem
+henon_prob = CLVProblem(henon_demo.prob)  # convert it to CLVProblem
+henon_angles = @time get_angles(init(henon_prob))
+
+lozi_prob = CLVProblem(LyapunovExponents.lozi_map(num_attr=100000).prob)
+lozi_angles = @time get_angles(init(lozi_prob))
+
+using Plots
+plt = plot(xlabel="Angle [pi/2 rad]", ylabel="Density", legend=:topleft)
+stephist!(plt, henon_angles,
+          bins=1000, normalize=true, linecolor=1, label="Henon")
+stephist!(twinx(plt), lozi_angles,
+          bins=1000, normalize=true, linecolor=2, label="Lozi")
+plt
