ee276618d833104cd90ae8ac47c7fb09f7e83d14
Takafumi Arakaki <aka.tkf@gmail.com>
2018-02-08 01:23:01 -0800

    Don't Pkg.free("LyapunovExponents")

--- misc/use-latest-versions.jl
+++ misc/use-latest-versions.jl
@@ -1,5 +1,10 @@
-println("Pkg.free(keys(Pkg.installed()))")
-Pkg.free(collect(keys(Pkg.installed())))
+if Base.VERSION >= v"0.7"
+    using Pkg
+end
+
+println("Pkg.free(... all packages ...)")
+Pkg.free(collect(Iterators.filter(x -> x != "LyapunovExponents",
+                                  keys(Pkg.installed()))))
 println("Pkg.resolve()")
 Pkg.resolve()
 println("Pkg.update()")
