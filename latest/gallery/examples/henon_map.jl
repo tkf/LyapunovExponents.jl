6989d5af7e6d98e3d6ce250ea3ac2c3f7776be81
autodocs <autodocs>
2018-02-16 11:43:48 +0000

    build based on d28550e

--- latest/gallery/examples/henon_map.jl
+++ latest/gallery/examples/henon_map.jl
@@ -1,4 +1,4 @@
 using LyapunovExponents
 using Plots
 demo = solve!(LyapunovExponents.henon_map(); progress=1)
-plot(demo)
+plt = plot(demo)
--- latest/gallery/examples/van_der_pol.jl
+++ latest/gallery/examples/van_der_pol.jl
@@ -1,4 +1,4 @@
 using LyapunovExponents
 using Plots
 demo = solve!(LyapunovExponents.van_der_pol(); progress=1)
-plot(demo)
+plt = plot(demo)
