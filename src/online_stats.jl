d688d0268a82381731fada09224cde3c43beec39
Takafumi Arakaki <aka.tkf@gmail.com>
2018-03-12 16:27:53 -0700

    Update to: OnlineStatsBase 0.6

--- src/online_stats.jl
+++ src/online_stats.jl
@@ -7,7 +7,7 @@ struct VecMean <: ExactStat{1}
     VecMean(μs::Vector{Float64}) = new(μs)
 end
 VecMean(len::Integer) = VecMean(zeros(Float64, len))
-OnlineStatsBase._fit!(o::VecMean, x::VectorOb, γ::Float64) = smooth!(o.μs, x, γ)
+OnlineStatsBase.fit!(o::VecMean, x::VectorOb, γ::Float64) = smooth!(o.μs, x, γ)
 Base.mean(o::VecMean) = value(o)
 
 
@@ -24,14 +24,14 @@ end
 VecVariance(len::Integer) = VecVariance(zeros(Float64, len),
                                         zeros(Float64, len))
 
-function OnlineStats._fit!(o::VecVariance, x::VectorOb, γ::Float64)
+function OnlineStatsBase.fit!(o::VecVariance, x::VectorOb, γ::Float64)
     o.nobs += 1
     o.tmp .= smooth.(o.μs, x, γ)
     o.σ2s .= smooth.(o.σ2s, (x .- o.μs) .* (x .- o.tmp), γ)
     o.tmp, o.μs = o.μs, o.tmp
 end
 
-OnlineStatsBase._value(o::VecVariance) =
+OnlineStatsBase.value(o::VecVariance) =
     o.nobs < 2 ? fill(NaN, size(o.μs)) : o.σ2s .* unbias(o)
 Base.var(o::VecVariance) = value(o)
 Base.std(o::VecVariance) = sqrt.(var(o))
