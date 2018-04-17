13707a8c9df8f9717d280554aee6fd70fa0d532b
Takafumi Arakaki <aka.tkf@gmail.com>
2018-04-16 19:49:53 -0700

    Define .n field instead of .nobs in custom OnlineStat

--- src/online_stats.jl
+++ src/online_stats.jl
@@ -6,12 +6,12 @@ import OnlineStatsBase
 mutable struct VecMean{W} <: OnlineStat{VectorOb}
     μs::Vector{Float64}
     weight::W
-    nobs::Int
+    n::Int
 end
 VecMean(μs::Vector{Float64}; weight = EqualWeight()) = VecMean(μs, weight, 0)
 VecMean(len::Integer) = VecMean(zeros(Float64, len))
 OnlineStatsBase._fit!(o::VecMean, x::VectorOb) =
-    smooth!(o.μs, x, o.weight(o.nobs += 1))
+    smooth!(o.μs, x, o.weight(o.n += 1))
 Base.mean(o::VecMean) = value(o)
 
 
@@ -20,7 +20,7 @@ mutable struct VecVariance{W} <: OnlineStat{VectorOb}
     μs::Vector{Float64}
     tmp::Vector{Float64}
     weight::W
-    nobs::Int
+    n::Int
 end
 function VecVariance(σ2s::Vector{Float64}, μs::Vector{Float64};
                      weight = EqualWeight())
@@ -31,17 +31,14 @@ VecVariance(len::Integer) = VecVariance(zeros(Float64, len),
                                         zeros(Float64, len))
 
 function OnlineStatsBase._fit!(o::VecVariance, x::VectorOb)
-    γ = o.weight(o.nobs += 1)
+    γ = o.weight(o.n += 1)
     o.tmp .= smooth.(o.μs, x, γ)
     o.σ2s .= smooth.(o.σ2s, (x .- o.μs) .* (x .- o.tmp), γ)
     o.tmp, o.μs = o.μs, o.tmp
 end
 
 OnlineStatsBase.value(o::VecVariance) =
-    o.nobs < 2 ? fill(NaN, size(o.μs)) : o.σ2s .* unbias(o)
+    o.n < 2 ? fill(NaN, size(o.μs)) : o.σ2s .* unbias(o)
 Base.var(o::VecVariance) = value(o)
 Base.std(o::VecVariance) = sqrt.(var(o))
 Base.mean(o::VecVariance) = o.μs
-
-# This is required for OnlineStats 0.15.3:
-OnlineStats.nobs(o::VecVariance) = o.nobs
