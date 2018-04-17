cd521ccb7f6caa68b4564f5e2035f33d9bb9f77f
Takafumi Arakaki <aka.tkf@gmail.com>
2018-04-16 18:29:17 -0700

    Test against *DiffEq*.jl master

---
+++ misc/use-dev-versions.jl
@@ -0,0 +1,20 @@
+if Base.VERSION >= v"0.7"
+    using Pkg
+end
+
+dev_packages = ["DiffEqBase", "OrdinaryDiffEq", "StochasticDiffEq"]
+dont_free = vcat(dev_packages, ["LyapunovExponents"])
+
+info("Pkg.free(... all packages ...)")
+Pkg.free(collect(Iterators.filter(x -> ! (x in dont_free),
+                                  keys(Pkg.installed()))))
+for name in dev_packages
+    info("Pkg.add($name)")
+    Pkg.add(name)  # to make the following work
+    info("Pkg.checkout($name)")
+    Pkg.checkout(name)
+end
+info("Pkg.resolve()")
+Pkg.resolve()
+info("Pkg.update()")
+Pkg.update()
