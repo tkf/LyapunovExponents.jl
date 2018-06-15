a48469b23fc7f795d8d0c631761b2cf365c31d1b
Takafumi Arakaki <aka.tkf@gmail.com>
2018-06-15 00:21:18 -0700

    Fix show-versions.jl for 0.7

--- misc/show-versions.jl
+++ misc/show-versions.jl
@@ -1,4 +1,12 @@
-for line in split(strip(readstring("REQUIRE")), '\n')[2:end]
+if Base.VERSION < v"0.7-"
+    _readstring = readstring
+else
+    _readstring = f -> read(f, String)
+    using Pkg
+end
+
+packages = Pkg.installed()
+for line in split(strip(_readstring("REQUIRE")), '\n')[2:end]
     name = split(line)[1]
-    println(name, "\t", Pkg.installed(name))
+    println(name, "\t", get(packages, name, ""))
 end
