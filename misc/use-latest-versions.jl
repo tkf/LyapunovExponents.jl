91e5991a541be10973a5eab4091a2edc7ff7a17e
Takafumi Arakaki <aka.tkf@gmail.com>
2018-06-14 23:22:29 -0700

    Add a workaround in use-latest-versions.jl for 0.7

--- misc/use-latest-versions.jl
+++ misc/use-latest-versions.jl
@@ -1,10 +1,24 @@
-if Base.VERSION >= v"0.7"
+if Base.VERSION < v"0.7-"
+    macro info(x)
+        :(info($(esc(x))))
+    end
+else
     using Pkg
 end
 
 println("Pkg.free(... all packages ...)")
-Pkg.free(collect(Iterators.filter(x -> x != "LyapunovExponents",
-                                  keys(Pkg.installed()))))
+try
+    Pkg.free(collect(Iterators.filter(x -> x != "LyapunovExponents",
+                                      keys(Pkg.installed()))))
+catch
+    # A workaround for Julia 0.7
+    for name in keys(Pkg.installed())
+        try
+            @info "Pkg.free($name)"
+            Pkg.free(name)
+        end
+    end
+end
 println("Pkg.resolve()")
 Pkg.resolve()
 println("Pkg.update()")
